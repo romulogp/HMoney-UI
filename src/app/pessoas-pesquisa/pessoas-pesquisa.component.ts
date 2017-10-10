d503859917e6281ae1ee524d87240a78a7491882
Rômulo Göelzer Portolann <romulogoelzer@gmail.com>
2017-10-10 01:06:14 -0300

    Componente pesquisa pessoas

--- src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
+++ src/app/pessoas-pesquisa/pessoas-pesquisa.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./pessoas-pesquisa.component.css']
 })
 export class PessoasPesquisaComponent {
-
+  pessoas = [
+    { nome: 'Manoel Pinheiro', cidade: 'Uberlândia', estado: 'MG', ativo: true },
+    { nome: 'Sebastião da Silva', cidade: 'São Paulo', estado: 'SP', ativo: false },
+    { nome: 'Carla Souza', cidade: 'Florianópolis', estado: 'SC', ativo: true },
+    { nome: 'Luís Pereira', cidade: 'Curitiba', estado: 'PR', ativo: true },
+    { nome: 'Vilmar Andrade', cidade: 'Rio de Janeiro', estado: 'RJ', ativo: false },
+    { nome: 'Paula Maria', cidade: 'Uberlândia', estado: 'MG', ativo: true }
+  ];
 }
