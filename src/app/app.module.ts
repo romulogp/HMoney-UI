2af17f1fc87842ca41ed5f905b18e784ced2af00
Rômulo Göelzer Portolann <romulogoelzer@gmail.com>
2017-10-20 22:04:36 -0200

    Validação do campo de descrição

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { InputMaskModule } from 'primeng/components/inputmask/inputmask';
 import { InputTextModule } from 'primeng/components/inputtext/inputtext';
@@ -33,6 +34,7 @@ import { PessoaCadastroComponent } from './pessoa-cadastro/pessoa-cadastro.compo
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    FormsModule,
     InputMaskModule,
     InputTextModule,
     InputTextareaModule,
